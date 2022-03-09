30b7c75bfe610b6bd5b0d2803223c8f6bdba1131
2017_aleksandraas <2017_aleksandraas@LAB27-9.school.npmg.org>
2022-03-09 12:52:17 +0200

    first

---
+++ Daskalnik/src/main/java/school/DaskalnikApplication.java
@@ -0,0 +1,13 @@
+package school;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaskalnikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaskalnikApplication.class, args);
+	}
+
+}
